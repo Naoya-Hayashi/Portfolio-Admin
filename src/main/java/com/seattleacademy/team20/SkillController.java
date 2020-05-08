d520041a566f32f2056db522947bf1e5e90d44b0
Naoya-Hayashi <62989057+Naoya-Hayashi@users.noreply.github.com>
2020-05-08 19:21:07 +0900

    sql

--- src/main/java/com/seattleacademy/team20/SkillController.java
+++ src/main/java/com/seattleacademy/team20/SkillController.java
@@ -1,33 +1,203 @@
 package com.seattleacademy.team20;
 
-
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Locale;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 /**
  * Handles requests for the application home page.
  */
 @Controller
-public class SkillController {
+public class SkillController<Skill> {
 
-	private static final Logger logger = LoggerFactory.getLogger(SkillController.class);
+  private static final Logger logger = LoggerFactory.getLogger(SkillController.class);
+  @Autowired
+  private JdbcTemplate jdbcTemplate;
 
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "/upload", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
+  /**
+   * Simply selects the home view to render by returning its name. * @throuws
+   * IOException
+   */
+  @RequestMapping(value = "/upload", method = RequestMethod.GET)
+  public String uploadSkill(Locale locale, Model model) {
+    logger.info("Welcome home! The client locale is {}.", locale);
 
+    try {
+      initialize();
+    } catch (IOException e) {
+      // TODO 自動生成された catch ブロック
+      e.printStackTrace();
+    }
+    ;
 
+    List<Skills> skills = selectskills();
+    uploadSkill(skills);
 
-		return "skill-upload";
-	}
+    return "skill-upload";
+  }
 
+  // ここからタスク10
+  //
+  // Listの宣言
+  public List<Skills> selectskills() {
+    // sequel proで作ったテーブルからデータを取得する文字列をsqlという変数に入れている
+    final String sql = "select * from skills";
+
+    return jdbcTemplate.query(sql, new RowMapper<Skills>() {
+      public Skills mapRow(ResultSet rs, int rowNum) throws SQLException {
+        return new Skills(rs.getString("id"), rs.getString("category"), rs.getString("name"), rs.getInt("score"));
+
+      }
+    });
+  }
+
+  private FirebaseApp app;
+
+  //
+  // SDKの初期化
+  public void initialize() throws IOException {
+    FileInputStream refreshToken = new FileInputStream(
+        "/Users/nao8/Downloads/develop-protfolio2020121-firebase-adminsdk-qcr8z-2b0605b198.json");
+    FirebaseOptions options = new FirebaseOptions.Builder().setCredentials(GoogleCredentials.fromStream(refreshToken))
+        .setDatabaseUrl("https://develop-protfolio2020121.firebaseio.com/").build();
+    app = FirebaseApp.initializeApp(options, "other");
+  }
+
+  //
+  public void uploadSkill(List<Skills> skills) {
+    // データの保存
+    final FirebaseDatabase database = FirebaseDatabase.getInstance(app);
+    DatabaseReference ref = database.getReference("skills");
+    //
+    // Map型のリストを作る。MapはStringで聞かれたものに対し、Object型で返すようにしている
+    List<Map<String, Object>> DataList = new ArrayList<Map<String, Object>>();
+    Map<String, Object> map;
+    Map<String, List<Skills>> skillMap = skills.stream().collect(Collectors.groupingBy(Skills::getSkillId));
+    for (Map.Entry<String, List<Skills>> entry : skillMap.entrySet()) {
+      map = new HashMap<>();
+      map.put("category", entry.getKey());
+      map.put("skill", entry.getValue());
+      DataList.add(map);
+
+      // 非互換オペランド型 String と intとは？↑
+      //
+      ref.setValue(DataList, new DatabaseReference.CompletionListener() {
+        @Override
+        public void onComplete(DatabaseError databaseError, DatabaseReference databaseReference) {
+          if (databaseError != null) {
+            System.out.println("Data could be saved" + databaseError.getMessage());
+          } else {
+            System.out.println("Data save successfully.");
+          }
+        }
+      });
+    }
+  }
 }
+
+//　	タスク１０
+//	private FirebaseApp app;
+//
+//		public void initialize() throws IOException {
+//		FileInputStream refreshToken = new FileInputStream("/Users/nao8/Downloads/develop-protfolio2020121-firebase-adminsdk-qcr8z-2b0605b198.json");
+//			FirebaseOptions options = new FirebaseOptions.Builder()
+//					.setCredentials(GoogleCredentials.fromStream(refreshToken))
+//					.setDatabaseUrl("https://develop-protfolio2020121.firebaseio.com/")
+//					.build();
+//			app = FirebaseApp.initializeApp(options,"other");
+//	}
+//
+//		public void uploadSkill() {
+//			final FirebaseDatabase database = FirebaseDatabase.getInstance(app);
+//			DatabaseReference ref = database.getReference("skill-categories");
+//
+//
+//
+//	}
+//
+//
+//	}
+//
+//}
+
+//
+//	@Autowired
+//	private JdbcTemplate jdbcTemplate;
+//	//list 宣言
+//	public List<Skill> selectSkills(){
+//		final String sql ="select * from skills";
+//		//fromはmySQLのテーブル名かと
+//		return jdbcTemplate.quary(sql, new RowMapper<Skill>() {
+//
+//			public Skills mapRow(ResultSet rs, int rowNum) throws SQLException{
+//				return new  Skills(rs.getInt("id"), rs.getString("category"),
+//						rs.getString("name"), rs.getString("score"));
+//			}
+//		});
+//	  }
+//
+//	private FirebaseApp  app;
+//	//SDK初期化
+//	public void initialize() throws IOException {
+//		File authFile = new File(getClass().getClassLoader().getResource("Authentication.json") .getFile());
+//		FileInputStream
+//		FirebaseOptions options = new FirebaseOptions.Builder()
+//				.setCredentials(GoogleCredentials.getApplicationDefault())
+//				.setDatabaseUrl("https://test-mysql-pull.firebaseio.com/")
+//				.build();
+//		app = FirebaseApp.initializeApp(options);
+//
+//	}
+//
+////	public void uploadSkill(List<Skill> skills) {
+////
+////
+////
+////	final FirebaseDatabase database = FirebaseDatabase.getInstance(app);
+////	DatabaseReference reference = database.getReference("skills");
+////
+////	List<Map<String, Object>> dataList = new ArrayList<Map<String , Object>>();
+////	Map<String, Object> dataMap;
+////	for(Skill skill : skills ) {
+////		dataMap = new HashMap<>();
+////		dataMap.put("skills", skills.stream()
+////				.collect(Collectors.toList()));
+////		dataList.add(dataMap);
+////	}
+////	reference.setValue(dataList, new DatabaseReference.CompletionListener() {
+////
+////		@Override
+////		public void onComplete(DatabaseError databaseError, DatabaseReference databaseReference) {
+////			// TODO 自動生成されたメソッド・スタブ
+////			if(databaseError != null) {
+////				System.out.println("Data could be saved" + databaseError.getMessage());
+////			} else {
+////				System.out.println("Data save successfully.");
+////			}
+////		}
+////	});
+////  }
---
+++ src/main/java/com/seattleacademy/team20/Skills.java
@@ -0,0 +1,59 @@
+package com.seattleacademy.team20;
+
+public class Skills {
+
+  private String id;
+  private String category;
+  private String name;
+  private int score;
+
+  public Skills(String id, String category, String name, int score) {
+    // TODO 自動生成されたコンストラクター・スタブ
+    this.id = id;
+    this.category = category;
+    this.name = name;
+    this.score = score;
+  }
+
+  public String getId() {
+
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getSkillId() {
+    return category;
+  }
+
+  public void setSkillId(String category) {
+    this.category = category;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public int getScore() {
+    return score;
+  }
+
+  public void setScore(int score) {
+    this.score = score;
+  }
+
+//	public String getCategory() {
+//		return category;
+//	}
+//
+//	public void setCategory(String category) {
+//		this.category = category;
+//	}
+
+}
